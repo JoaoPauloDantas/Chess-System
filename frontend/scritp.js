061e7c439581b9748c573fce8e9dea7a64530cd1
João Paulo Dantas <120749055+JoaoPauloDantas@users.noreply.github.com>
2026-06-05 09:45:24 -0300

    Update scritp.js

--- frontend/scritp.js
+++ frontend/scritp.js
@@ -1,1 +1,38 @@
 
+const board = document.getElementById("board");
+
+const pieces = [
+
+"♜","♞","♝","♛","♚","♝","♞","♜",
+"♟","♟","♟","♟","♟","♟","♟","♟",
+
+"","","","","","","","",
+"","","","","","","","",
+"","","","","","","","",
+"","","","","","","","",
+
+"♙","♙","♙","♙","♙","♙","♙","♙",
+"♖","♘","♗","♕","♔","♗","♘","♖"
+
+];
+
+for(let row=0; row<8; row++){
+
+    for(let col=0; col<8; col++){
+
+        const square = document.createElement("div");
+
+        square.classList.add("square");
+
+        if((row+col)%2===0){
+            square.classList.add("white");
+        }else{
+            square.classList.add("black");
+        }
+
+        square.textContent =
+            pieces[row*8+col];
+
+        board.appendChild(square);
+    }
+}
